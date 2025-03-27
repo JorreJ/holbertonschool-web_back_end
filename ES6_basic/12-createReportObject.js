3e5497e0c51904d2dd5e24a0d3b6570eee703eb8
JorreJ <jade.jorre@gmail.com>
2025-03-27 11:19:24 +0100

    create file for task 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees: employeesList,
+        getNumberOfDepartments(employeesList) {
+            let num = 0;
+            for (let x in employeesList)
+                num += 1;
+            return num;
+        }
+    }
+}
