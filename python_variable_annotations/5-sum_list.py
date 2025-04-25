98ec65f3f36f2b8267f955b721aae2ad61aaa698
JorreJ <jade.jorre@gmail.com>
2025-04-25 21:54:21 +0200

    modify list type annotation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
+from typing import List
 """
 Function to calculate the sum of all float elements in a list.
 Useful for aggregating numeric data from a sequence.
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Computes the sum of a list of floating-point numbers.
 
