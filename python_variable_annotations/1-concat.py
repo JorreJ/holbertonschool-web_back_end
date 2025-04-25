8ce9fd359cc249618e7cba62c3e4396d0dbcc2ce
JorreJ <jade.jorre@gmail.com>
2025-04-25 11:46:55 +0200

    create file for task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Function to concatenate two strings.
+Useful for combining text values or building dynamic messages.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 followed by str2.
+    """
+    return str1 + str2
