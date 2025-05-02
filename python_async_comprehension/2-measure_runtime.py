2df967ee3de65881ffcc33cae85c9ad3bb861b42
JorreJ <jade.jorre@gmail.com>
2025-05-02 08:45:24 +0200

    add documentation to file for task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This script defines an asynchronous function that measures the total runtime
+of executing multiple async comprehensions concurrently using asyncio.gather.
+It demonstrates the benefits of asynchronous
+execution in reducing total wait time.
+"""
 
 import asyncio
 import time
@@ -6,6 +12,14 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    Measures the total runtime of running four
+    async_comprehension calls concurrently.
+
+    Returns:
+        float: The elapsed time in seconds to
+        complete all four asynchronous tasks.
+    """
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
     end = time.perf_counter()
