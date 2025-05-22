d3353b9a1f6b13899c8df9294e6e9c0fe1b36f10
JorreJ <jade.jorre@gmail.com>
2025-05-22 13:30:25 +0200

    change command to process write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name ?\n');
-process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
-    console.log(`Your name is: ${input}`);
-})
-process.stdin.on('close', function() {
-    console.log('This important software is now closing');
-})
+process.stdin.on('readable', () => {
+    const input = process.stdin.read();
+    if (input) {
+        process.stdout.write(`Your name is: ${input}`);
+    }
+    
+});
+process.stdin.on('close', () => {
+    process.stdout.write('This important software is now closing\n');
+});
