1a2fe406b8529ba1a20ce14879b6cf584c9e54c8
JorreJ <jade.jorre@gmail.com>
2025-05-22 11:39:08 +0200

    try process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('Welcome to Holberton School, what is your name ?\n', (input) => {
+process.stdout.write('Welcome to Holberton School, what is your name ?\n');
+process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
     console.log(`Your name is: ${input}`);
-    rl.close();
-    //rl.on('close', function() {
-    //    console.log('This important software is now closing');
-    //    process.exit(0);
-    //});
-});
+}).on('close', function() {
+    console.log('This important software is now closing');
+    process.exit();
+})
