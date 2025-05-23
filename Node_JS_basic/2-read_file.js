4cf8fe348f9e7512f3cec248fc42989478083803
JorreJ <jade.jorre@gmail.com>
2025-05-23 11:23:08 +0200

    add if statement to for-in loop and fix indentation

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,30 @@
 const fs = require('node:fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const Students = lines.slice(1);
-        const fields = {}
-        Students.forEach((Student) => {
-            infos = Student.split(',').map((info) => info.trim());
-            if (infos.length === 4) {
-                const field = infos[3];
-                const firstName = infos[0];
-                if (!fields[field]) {
-                    fields[field] = []
-                }
-                fields[field].push(firstName);
-            }
-        });
-        console.log(`Number of students: ${Students.length}`);
-        for (let field in fields)
-            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`)
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const Students = lines.slice(1);
+    const fields = {};
+    Students.forEach((Student) => {
+      const infos = Student.split(',').map((info) => info.trim());
+      if (infos.length === 4) {
+        const field = infos[3];
+        const firstName = infos[0];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+      }
+    });
+    console.log(`Number of students: ${Students.length}`);
+    for (const field in fields) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
-    catch (err) {
-        throw new Error('Cannot load the database');
-    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
-module.exports = countStudents
+module.exports = countStudents;
